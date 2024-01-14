314845702d68ba11ebadd2f5d745e93ed75baa95
Shrikant Chine <shrikant.chine@gmail.com>
2024-01-14 23:08:32 +0530

    Adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import Request from "./components/request";
 import Response from "./components/response";
+import { FaJedi } from "react-icons/fa";
+import { Navbar } from "react-bootstrap";
 
 function App() {
   const [responseBody, setResponseBody] = useState("");
   const [responseHeaders, setResponseHeaders] = useState([]);
   return (
-    <>
-      <h2 className="px-5 pt-1">React Client</h2>
+    <div>
+      <Navbar bg="light" data-bs-theme="light">
+        <Navbar.Brand className="px-5">
+          <FaJedi size={38} /> <span className="px-3">React Client</span>
+        </Navbar.Brand>
+      </Navbar>
+      {/* <div className="d-flex px-5 pt-2">
+        <FaJedi size={40} />
+        <h2 className="px-3 pt-1">React Client</h2>
+      </div> */}
       <div className="p-5 pt-4">
         <Request
           setResponseBody={setResponseBody}
@@ -18,7 +28,7 @@ function App() {
           <Response body={responseBody} headers={responseHeaders} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
